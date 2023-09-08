18a4e3bb08b70b3b4632adaa4f64ae1a263c20f9
Vladislav <shishkin241993@gmail.com>
2023-09-08 17:29:33 -0400

    BlogProject

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+import CardList from "./components/CardList/CardList";
+import CategoryList from "./components/CategoryList/CategoryList";
+import Menu from "./components/Menu/Menu";
+import Featured from "./components/featured/Featured";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
