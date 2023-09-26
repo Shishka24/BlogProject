d68dd8caa8ce355912117a194c48a729784c5dd0
Vladislav <shishkin241993@gmail.com>
2023-09-25 21:10:35 -0400

    BlogProject/singIn-logIn

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
-  const status = "notAuth";
+  const status = "Auth";
   return (
     <>
       {status === "notAuth" ? (
