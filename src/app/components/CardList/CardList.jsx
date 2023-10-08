981b706eb306db446365f49dd2a08c43d6cd0da7
Vladislav <shishkin241993@gmail.com>
2023-10-08 00:06:14 -0400

    Prisma/backend/api

--- src/app/components/CardList/CardList.jsx
+++ src/app/components/CardList/CardList.jsx
@@ -3,7 +3,18 @@ import styles from "./cardList.module.css";
 import Pagination from "../Pagination/Pagination";
 import Card from "../card/Card";
 
-function CardList() {
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+  return res.json();
+};
+
+const CardList = async ({ page }) => {
+  const data = await getData(page);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
@@ -16,6 +27,6 @@ function CardList() {
       <Pagination />
     </div>
   );
-}
+};
 
 export default CardList;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,14 @@ import Featured from "./components/featured/Featured";
 import Footer from "./components/footer/Footer";
 import styles from "./homepage.module.css";
 
-export default function Home() {
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
   return (
     <div className={styles.container}>
       <Featured />
       <CategoryList />
       <div className={styles.content}>
-        <CardList />
+        <CardList page={page} />
         <Menu />
       </div>
     </div>
